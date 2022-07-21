f21a1fe9a5458b4642ed3b2b693a7cff8267c3b0
toth_szilvia <szylwy@gmail.com>
2022-07-22 00:02:06 +0200

    add contact cs and contactcontroller

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagerAspRestfulMvc.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string[] Get()
+        {
+            return new string[]
+            {
+                "Hello",
+                "World"
+            };
+        }
+    }
+}
